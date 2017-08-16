5442ba373ef9d374c206cb551d234f769000cbdb
InSouL <dimaxdecibel@gmail.com>
2017-08-16 16:41:01 +0500

    Правки по классам

--- frontend/views/students-class/classes.php
+++ frontend/views/students-class/classes.php
@@ -19,7 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="col-sm-3" ></div>
 <div class="col-sm-6" >
 
-    <div class="raspisanie-table mb-30px">
+    <div class="raspisanie-table mb-30px overy">
         <div class="rt-table">
             <table>
                 <thead class="rt-header"><tr>
